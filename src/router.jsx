b74325386f9c5d51793ed5f91f03ac67ca06db21
michellevillegasv <michelle.villegas@correo.unimet.edu.ve>
2023-06-10 16:14:20 -0400

    Update Login and Route

--- src/router.jsx
+++ src/router.jsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./views/HomePage";
+import Login from "./views/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />}/>
     </Route>
   )
 );
