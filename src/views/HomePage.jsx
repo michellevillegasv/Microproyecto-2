0e0babd73f925a65c9022cfa291336d6959c1868
michellevillegasv <michelle.villegas@correo.unimet.edu.ve>
2023-06-10 17:20:42 -0400

    API code

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,6 +1,19 @@
+import {useState,useEffect} from "react";
 import Hero from "../components/Hero";
+import { fetchMovieNow } from "../utils/now-playing-api";
 
 export default function HomePage() {
+  const[moviesNow, setMoviesNow]=useState("")
+
+  const getMoviesNow=async()=>{
+    const response=await fetchMovieNow();
+    
+    console.log(response);
+  }
+  useEffect(()=>{
+    getMoviesNow();
+  },[]);
+
   return (
     <div>
       <Hero />
