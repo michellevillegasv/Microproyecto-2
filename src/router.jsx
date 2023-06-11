e2aad0d337d3c0fec156173cf1f3f9454aabc04f
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-11 00:41:16 -0400

    Added components and improved HomePage

--- src/router.jsx
+++ src/router.jsx
@@ -4,12 +4,13 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "./components/Layout";
+import homePageloader from "./loaders/homePageLoader";
 import HomePage from "./views/HomePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} loader={homePageloader} />
     </Route>
   )
 );
