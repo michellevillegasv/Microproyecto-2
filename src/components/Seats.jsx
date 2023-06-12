331a25734d6f44ad049dc7573a797bf7fe634ca2
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-12 14:51:36 -0400

    Added registration action

--- src/components/Seats.jsx
+++ src/components/Seats.jsx
@@ -1,11 +1,18 @@
 import Seat from "./Seat";
 import styles from "./Seats.module.css";
 
-export default function Seats({ seats }) {
+/**
+ * @typedef {{
+ *   seats: import("./Seat").SeatProps[];
+ *   onSelect: (id: number, status: import("./Seat").SeatStatus) => void;
+ * }}
+ */
+
+export default function Seats({ seats, onSelect }) {
   return (
     <div className={styles.seats}>
       {seats.map(({ id, status }) => (
-        <Seat key={id} status={status} />
+        <Seat key={id} status={status} onClick={() => onSelect(id, status)} />
       ))}
     </div>
   );
--- src/loaders/reservationLoader.js
+++ src/loaders/reservationLoader.js
@@ -1,30 +1,33 @@
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { defer } from "react-router-dom";
 import { db } from "../firebaseConfig";
 import { fetchMovie, getImageSource } from "../utils/movies";
 
 async function loadSeats(movieId) {
   const moviesRef = collection(db, "movies");
+
   const movieRef = doc(moviesRef, movieId);
   const movieDoc = await getDoc(movieRef);
   if (movieDoc.exists()) {
-    const seats = new Array(20).map((_, index) => ({
+    return movieDoc.data()?.seats || [];
+  } else {
+    const seats = new Array(20).fill(null).map((_, index) => ({
       id: index,
       status: "available",
     }));
-    addDoc(moviesRef, {
+    const data = {
       id: movieId,
       seats,
-    });
+    };
+    setDoc(movieRef, data);
     return seats;
-  } else {
-    return movieDoc.data()?.seats || [];
   }
 }
 
 async function loadMovie(movieId) {
   const data = await fetchMovie(movieId);
   return {
+    id: data.id,
     banner: getImageSource(data.backdrop_path),
     poster: getImageSource(data.poster_path),
     title: data.title,
